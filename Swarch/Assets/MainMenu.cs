96d6a7adb96254cee16bfb76a539ee3a5611f220
JayhawkForLife <jayhawkfan999@gmail.com>
2014-04-15 16:14:04 -0700

    Updated GUI

--- Swarch/Assets/MainMenu.cs
+++ Swarch/Assets/MainMenu.cs
@@ -8,12 +8,23 @@ public class MainMenu : MonoBehaviour {
 	void OnGUI()
 	{
 		userName = GUI.TextField(new Rect(200, 75, 200, 20), userName, 25);
+
 		password = GUI.PasswordField(new Rect(200, 100, 200, 20), password, '*', 25);
 		if(GUI.Button(new Rect(270, 130, 50, 20), "Login"))
 		{
 			Application.LoadLevel(1);
 		}
 
+	}
+
+	void awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+
+	public string getUserName()
+	{
+		return userName;
 	}
 
 
